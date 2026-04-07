872a25ea7a5dcdca8e58cfb62449ed1a3266fd4f
j-goodman <johncurtisgoodman@gmail.com>
2026-04-07 19:57:36 -0400

    Add fetch request and data population using hardcoded example.

--- script.js
+++ script.js
@@ -7,3 +7,18 @@ document.addEventListener("keypress", (event) => {
         console.log("Enter key pressed.")
     }
 })
+
+async function requestCountryInfo (countryName) {
+    const response = await fetch(`https://restcountries.com/v3.1/name/${countryName}`)
+    const country = await response.json()
+    addInformationToPage(country[0])
+}
+
+const addInformationToPage = (country) => {
+    console.log(country)
+    countryName.innerText = country.name.common
+    countryFlag.src = country.flags.svg
+    countryDescription.innerText = `${country.name.official} is a country in ${country.subregion} with a population of ${country.population.toLocaleString()}.`
+}
+
+requestCountryInfo("Ecuador")

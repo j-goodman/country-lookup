a80fddb1472f5a413dc0c33125e64e8d45fa98a4
j-goodman <johncurtisgoodman@gmail.com>
2026-04-07 20:48:12 -0400

    Added user input of country names.

--- script.js
+++ script.js
@@ -1,24 +1,34 @@
 const countryName = document.getElementById("country-name")
 const countryFlag = document.getElementById("country-flag")
 const countryDescription = document.getElementById("country-description")
+const inputBox = document.getElementById("input-box")
 
 document.addEventListener("keypress", (event) => {
     if (event.key === "Enter") {
-        console.log("Enter key pressed.")
+        requestCountryInfo(inputBox.value)
     }
 })
 
 async function requestCountryInfo (countryName) {
     const response = await fetch(`https://restcountries.com/v3.1/name/${countryName}`)
-    const country = await response.json()
-    addInformationToPage(country[0])
+    const countries = await response.json()
+
+    let correctCountry = countries[0]
+
+    for (let i = 0; i < countries.length; i++) {
+        console.log(countries[i])
+        if (countries[i].name.common === countryName) {
+            correctCountry = countries[i]
+        }
+    }
+
+    addInformationToPage(correctCountry)
 }
 
 const addInformationToPage = (country) => {
-    console.log(country)
+    window.country = country
     countryName.innerText = country.name.common
     countryFlag.src = country.flags.svg
-    countryDescription.innerText = `${country.name.official} is a country in ${country.subregion} with a population of ${country.population.toLocaleString()}.`
+    countryFlag.alt = country.flags.alt
+    countryDescription.innerText = `${country.name.official} is a country in ${country.subregion} with a population of ${country.population.toLocaleString()}. The capital of ${country.name.common} is ${country.capital}.`
 }
-
-requestCountryInfo("Ecuador")

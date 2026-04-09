605db38300ea4c7994d245da81b4bbe94ac041c7
j-goodman <johncurtisgoodman@gmail.com>
2026-04-08 22:43:25 -0400

    Replace global variables with page object.

--- script.js
+++ script.js
@@ -1,35 +1,42 @@
-const countryName = document.getElementById("country-name")
-const countryFlag = document.getElementById("country-flag")
-const countryDescription = document.getElementById("country-description")
-const inputBox = document.getElementById("input-box")
+const page = {
+    countryName: document.getElementById("country-name"),
+    countryFlag: document.getElementById("country-flag"),
+    countryDescription: document.getElementById("country-description"),
+    inputBox: document.getElementById("input-box"),
+    mainSection: document.getElementById("main-section"),
+    aboutSection: document.getElementById("about-section"),
+    mainButton: document.getElementById("main-section-button"),
+    aboutButton: document.getElementById("about-section-button")
+}
 
-const mainSection = document.getElementById("main-section")
-const aboutSection = document.getElementById("about-section")
-const mainButton = document.getElementById("main-section-button")
-const aboutButton = document.getElementById("about-section-button")
-
-mainButton.addEventListener("click", () => {
-    mainSection.classList.remove("invisible")
-    aboutSection.classList.add("invisible")
-    mainButton.classList.add("active")
-    aboutButton.classList.remove("active")
+page.mainButton.addEventListener("click", () => {
+    page.mainSection.classList.remove("invisible")
+    page.aboutSection.classList.add("invisible")
+    page.mainButton.classList.add("active")
+    page.aboutButton.classList.remove("active")
 })
 
-aboutButton.addEventListener("click", () => {
-    aboutSection.classList.remove("invisible")
-    mainSection.classList.add("invisible")
-    aboutButton.classList.add("active")
-    mainButton.classList.remove("active")
+page.aboutButton.addEventListener("click", () => {
+    page.aboutSection.classList.remove("invisible")
+    page.mainSection.classList.add("invisible")
+    page.aboutButton.classList.add("active")
+    page.mainButton.classList.remove("active")
 })
 
 document.addEventListener("keypress", (event) => {
     if (event.key === "Enter") {
-        requestCountryInfo(inputBox.value)
+        requestCountryInfo(page.inputBox.value)
     }
 })
 
 async function requestCountryInfo (countryName) {
     const response = await fetch(`https://restcountries.com/v3.1/name/${countryName}`)
+    if (response.status === 404) {
+        console.log("!!!!!")
+        console.log(countryName)
+        page.countryName.innerText = "I am error."
+        return false
+    }
     const countries = await response.json()
 
     let correctCountry = countries[0]
@@ -44,8 +51,8 @@ async function requestCountryInfo (countryName) {
 }
 
 const addInformationToPage = (country) => {
-    countryName.innerText = country.name.common
-    countryFlag.src = country.flags.svg
-    countryFlag.alt = country.flags.alt
-    countryDescription.innerText = `${useThe(country.name.official) ? "The " : ""}${country.name.official} is a country in ${useThe(country.subregion) ? "the " : ""}${country.subregion} with a population of ${country.population.toLocaleString()}. Its capital is ${country.capital}.${country.landlocked ? ` ${country.name.common} is landlocked.` : ``}`
+    page.countryName.innerText = country.name.common
+    page.countryFlag.src = country.flags.svg
+    page.countryFlag.alt = country.flags.alt
+    page.countryDescription.innerText = `${useThe(country.name.official) ? "The " : ""}${country.name.official} is a country in ${useThe(country.subregion) ? "the " : ""}${country.subregion} with a population of ${country.population.toLocaleString()}. Its capital is ${country.capital}.${country.landlocked ? ` ${country.name.common} is landlocked.` : ``}`
 }
